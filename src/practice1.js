a3e75e9c133930a712d909585ae7d4cea19c2d38
di.qin <di.qin@thoughtworks.com>
2020-10-15 17:31:30 +0800

    [qd]: finish practice1.js and practice2.js

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const parseData = ({ data, column }) => {
+  const keys = column.map((cur) => cur.name);
+  return data.map((cur) =>
+    cur.reduce((result, field, index) => {
+      result[keys[index]] = field;
+      return result;
+    }, {})
+  );
+};
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+  sections.sort((a, b) => b.index - a.index);
+  sections.forEach((cur) => {
+    items.splice(cur.index, 0, cur.content);
+  });
+  return items;
+};
 export { inject };
